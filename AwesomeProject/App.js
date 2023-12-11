e65a0bb072794e679f13e25bad62c48d34ada37d
Jimmy Rashid <jimmyrashid22@gmail.com>
2023-12-11 14:20:29 -0800

    Moving second platform

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -5,7 +5,7 @@ import { StatusBar } from "expo-status-bar";
 import { Scene } from "./src/scene";
 import { Html } from "@react-three/drei";
 
-import * as STDLIB from 'three-stdlib'
+import * as STDLIB from "three-stdlib";
 
 const xChange = 0;
 const yChange = 0.526;
@@ -23,11 +23,44 @@ const styles = StyleSheet.create({
   },
 });
 
+export const MovingPlatform = () => {
+  var [xChangeMoving, setX] = useState(0);
+  var [yChangeMoving, setY] = useState(0.526);
+  var [zChangeMoving, setZ] = useState(0.245);
+
+  var [direction, setDirection] = useState("positive");
+
+  setTimeout(() => {
+    // {xChangeMoving < 1 ? setX(xChangeMoving + 0.05) : xChangeMoving > -1 ? setX(xChangeMoving - 0.05) : xChangeMoving}
+    // {zChangeMoving < 1 ? setZ(zChangeMoving + 0.4) : zChangeMoving > -1 ? setZ(zChangeMoving - 0.4) : zChangeMoving}
+    if (xChangeMoving < 2 && direction === "positive") {
+      setX(xChangeMoving + 0.05);
+      setZ(zChangeMoving + 0.025);
+    } else if (xChangeMoving >= 2 && direction === "positive") {
+      setDirection("negative");
+    }
+    if (xChangeMoving > -2 && direction === "negative") {
+      setX(xChangeMoving - 0.05);
+      setZ(zChangeMoving - 0.025);
+    } else if (xChangeMoving <= -2 && direction === "negative") {
+      setDirection("positive");
+    }
+  }, 20);
+  return (
+    <Scene
+      x={0 + xChangeMoving}
+      y={-1.68 + yChangeMoving}
+      z={0 + zChangeMoving}
+    />
+  );
+};
+
 export default function App() {
   return (
     <Canvas>
-      <Scene x={0} y={-1.68} z={0}/>
-      <Scene x={0 + xChange} y={-1.68 + yChange} z={0 + zChange} />
+      <Scene x={0} y={-1.68} z={0} />
+      {/* <Scene x={0 + xChange} y={-1.68 + yChange} z={0 + zChange} /> */}
+      <MovingPlatform />
     </Canvas>
   );
 }
