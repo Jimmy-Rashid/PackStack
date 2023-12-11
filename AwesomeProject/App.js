8f4e80c6c93dd35e022860fb67d69b788aaa2c7d
Jimmy Rashid <jimmyrashid22@gmail.com>
2023-12-11 09:31:05 -0800

    Updated starting platform

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,15 +1,17 @@
 import { Button, StyleSheet, Text, View } from "react-native";
 import { useState, useRef } from "react";
-import { StatusBar } from "expo-status-bar";
 import { Canvas, useFrame } from "@react-three/fiber";
+import { Html } from "@react-three/drei";
+import { StatusBar } from "expo-status-bar";
 import { Scene } from "./src/scene";
+import * as STDLIB from 'three-stdlib'
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: "white",
-    alignItems: "center",
     justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
   },
   text: {
     color: "black",
--- AwesomeProject/src/scene.tsx
+++ AwesomeProject/src/scene.tsx
@@ -1,10 +1,16 @@
 export function Scene() {
   return (
     <>
-      <mesh castShadow receiveShadow={true} scale={[0.86,0.6,0.86]} visible={true} position={[0,-1.68,0]} rotation={[0.4363323129985824,0.7853981633974483,0]} frustumCulled={false}>
-        <boxGeometry args={[2,1,2,1,1,1]} morphTargetsRelative={false} />
-        <meshStandardMaterial color={"#ff0000"} transparent={false} alphaHash={false} clipShadows={false} flatShading={false} fog={false} forceSinglePass={false} polygonOffset={false} stencilWrite={false} dithering={true} />
+      <mesh
+        scale={[0.86, 0.6, 0.86]}
+        position={[0, -1.68, 0]}
+        rotation={[0.4363323129985824, 0.7853981633974483, 0]}
+      >
+        <boxGeometry args={[2, 1, 2, 1, 1, 1]} />
+        <meshStandardMaterial color={"#b37100"} />
       </mesh>
-    <directionalLight position={[0.64,0.56,2.08]} /><ambientLight intensity={0.6} /></>
+      <directionalLight position={[0.64, 0.56, 2.08]} />
+      <ambientLight intensity={0.6} />
+    </>
   );
 }
